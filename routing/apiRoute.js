11f774dd33d50a1dfa8eb952db86bcdda416036c
Jose Castellanos <josecastellanos@Joses-MacBook-Pro-2.local>
2018-06-03 16:11:38 -0700

    routes are working just need logic

--- routing/apiRoute.js
+++ routing/apiRoute.js
@@ -2,40 +2,12 @@
 //A POST routes /api/friends. This will be used to handle incoming survey results. 
 //This route will also be used to handle the compatibility logic.
 // Dependinces 
-var express = require("express");
-var bodyParser = require("body-parser")
+var friendsList = require("../app/data/friends"); 
 
-// Sets up path to Express app 
-var app = express();
-var PORT = process.env.PORT || 3000;
-
-
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-var friends = [{
-    name: "Ahmed",
-    photo:"https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
-    scores: [1,3,4,5,6,7,8,1]
-}]
-//     // name:"",
-//     // photo:"",
-//     // scores:[1,2,3,3,4,5,6,7]
+module.exports = function(app){
+    app.get("/api/friends", function(req, res) {
+        res.json(friendsList);
+    })
+}
 
 
-// ]
-
-
-app.get("/", function(req,res) {
-    res.sendFile(path.join(_dirname, ))
-})
-
-
-app.get("/api/friends" ,function(req, res){
-    res.render
-})
-
-app.listen(PORT, function(){
-    console.log("listening on Local Host Port: " + PORT)
-});
--- server.js
+++ server.js
@@ -6,27 +6,15 @@ var path = require("path");
 
 // Sets up path to Express app 
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3366;
 
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
-app.get("/", function(req,res) {
-    res.sendFile(path.join(_dirname,"home.html"));
-    console.log("Your are in the Home page");
-});
-
-app.get("/survey", function(req,res){
-    res.sendFile(path.join(_dirname, "survey.html"))
-    console.log("You are in the Survey Page");
-})
-
-app.get("/api/friends" ,function(req, res){
-    res.render
-})
+require("./routing/apiRoute")(app);  
+require("./routing/htmlRoute")(app);  
 
 app.listen(PORT, function(){
     console.log("listening on Local Host Port: " + PORT)
