0579b3a9eb06f46c8cde23c306a82e7bf437638e
Jose Castellanos <josecastellanos@calvisitor-10-105-194-9.calvisitor.1918.berkeley.edu>
2018-06-06 09:41:46 -0700

    only one more thing nedded

--- routing/apiRoute.js
+++ routing/apiRoute.js
@@ -8,21 +8,24 @@ module.exports = function(app){
     app.get("/api/friends", function(req, res) {
         res.json(friendsList);
     })
+
+    app.post("/api/friends", function(req, res) {
+        // Send back the ID of the new todo
+        console.log(req.body);
+
+        // need logic to compare both 
+        // then res json info back 
+ 
+  });
 }
 
-
+ 
 // i think i need a post request here or in my html that will be on the modal
 // app.post("api/friends", function(req, res){
 
 // })
 
-// app.post("/api/friends", function(req, res) {
-//           // Send back the ID of the new todo
-//       res.json({ id: result.insertId });
 
-//       console.log({ id: result.insertId });
-   
-//     });
 
   
 
